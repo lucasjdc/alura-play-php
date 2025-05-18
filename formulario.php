dc305275e4b2121aae4dfbe10dec5628dd32f0f0
lucasjdc <lucasjpdc@gmail.com>
2025-05-18 18:55:12 -0300

    Limpando o código

--- formulario.php
+++ formulario.php
@@ -8,7 +8,7 @@ $video = [
     'url' => '',
     'title' => '',
 ];
-if ($id !== false) {
+if ($id !== false && $id !== null) {
     $statement = $pdo->prepare('SELECT * FROM videos WHERE id = ?;');
     $statement->bindValue(1, $id, PDO::PARAM_INT);
     $statement->execute();
@@ -18,26 +18,8 @@ if ($id !== false) {
 
 
 
-?><!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/estilos.css">
-    <link rel="stylesheet" href="../css/estilos-form.css">
-    <link rel="stylesheet" href="../css/flexbox.css">
-    <title>AluraPlay</title>
-    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
-</head>
-
-<body>
-
-    <!-- Cabecalho -->
-    <header>
+?>
+<?php require_once 'inicio-html.php'; ?>
 
         <nav class="cabecalho">
             <a class="logo" href="/"></a>
@@ -80,7 +62,4 @@ if ($id !== false) {
         </form>
 
     </main>
-
-</body>
-
-</html>
+<?php require_once 'fim-html.php'; ?>
